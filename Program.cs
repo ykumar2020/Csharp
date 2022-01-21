2c32ac9ba8e578fc3bb7f23018b7479be5c7cbb4
ykumar2020 <67843751+ykumar2020@users.noreply.github.com>
2022-01-21 00:24:31 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string text = System.IO.File.ReadAllText("text.txt");
+Console.WriteLine(text);
+Console.WriteLine("Dr Y. Kumar");
+Console.WriteLine("Version: {0}", Environment.Version.ToString()); 
